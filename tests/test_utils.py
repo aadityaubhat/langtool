dba5c660453bed12d748980a260b3dea043e526c
Aaditya Bhat <aadityaubhat@gmail.com>
2023-04-16 17:06:21 -0400

    Added folder structure

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+from toolify.utils import get_docstring, get_members
+
+def test_get_docstring():
+    """Test that get_docstring returns the docstring for an object."""
+    def test_function():
+        """This is a test function."""
+        pass
+    assert get_docstring(test_function) == "This is a test function."
+
+
+
+def test_get_members():
+    """Test that get_members returns a list of members of an object."""
+    class TestClass(object):
+        def test_method(self):
+            """This is a test method."""
+            pass
+
+        def _private_method(self):
+            pass
+
+    assert get_members(TestClass) == [('test_method', TestClass.test_method)]
---
+++ toolify/utils.py
@@ -0,0 +1,13 @@
+import inspect
+
+def get_docstring(obj):
+    """Return the docstring for an object, or None if no docstring is found."""
+    docstring = inspect.getdoc(obj)
+    if docstring is None:
+        docstring = inspect.getcomments(obj)
+    return docstring
+
+def get_members(obj, predicate=None):
+    """Return a list of members of an object, optionally filtered by a predicate."""
+    members = inspect.getmembers(obj, predicate)
+    return [member for member in members if not member[0].startswith('_')]
